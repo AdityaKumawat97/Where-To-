c4060a90e8042afd96d1aa31a2ff1c46498b32ad
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2021-09-07 17:18:52 +0530

    added styles and content to header

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import { CssBaseline, Grid } from "@material-ui/core";
 function App() {
   return (
     <>
-       
       <CssBaseline />
       <Header />
-      <Grid>
-        <List />
-        <Map />
-        <PlaceDetails />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4} style={{ border: "1px solid black" }}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8} style={{ border: "1px solid black" }}>
+          <Map />
+        </Grid>
       </Grid>
-        
     </>
   );
 }
